6a9abe466d861068c5fcb9849334262dd4632306
duff-a11y <joakim@axesslab.com>
2019-10-08 12:08:45 +0200

    Basic setup of website with improved theme

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+(function ($) {
+    'use strict';
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    $(".js-scroll-to-content").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+})(jQuery);
